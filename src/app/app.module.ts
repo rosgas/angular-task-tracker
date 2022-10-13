28fed997a6f92d30e44b5b7c5fc119c29e501dc0
Rossella Gasbarri <rossella@192-168-1-112.tpgi.com.au>
2022-10-13 15:23:39 +1000

    GET & DELETE tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -15,14 +16,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    NgbModule,
-    FontAwesomeModule
-  ],
+  imports: [BrowserModule, NgbModule, FontAwesomeModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
-import { from } from 'rxjs';
+//import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  getTask(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+  constructor(private http: HttpClient) {}
+
+  // getTask(): Observable<Task[]> {
+  //   const tasks = of(TASKS);
+  //   return tasks;
+  // }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
